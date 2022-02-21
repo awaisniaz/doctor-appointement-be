c3c25cef19c6191f092c9c74f00032e135c5294e
Awais Niaz <awais.niaz@mwanmobile.org>
2022-02-21 22:11:03 +0500

    Api fixes

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
 const app = express()
-
+const PORT = process.env.PORT || 3000;
 app.get('login',(req,res)=>{
     res.send('I am login endPoint ')
 })
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('😍 i am running')
 })
