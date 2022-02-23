86e4b12cb761465ee12ae560b558a5b595b6c828
Awais Niaz <awais.niaz@mwanmobile.org>
2022-02-23 22:26:05 +0500

    user token and password hash generation

--- router/user.js
+++ router/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {createUser} from "../controller/user";
+import {createUser,loginUser} from "../controller/user";
 import { authorize } from '../middleware/authorization'
 var router = express.Router()
 console.log("I am Awais Niaz")
 router.post('/create',authorize,createUser)
+router.get('/login',loginUser)
 
 export default router
