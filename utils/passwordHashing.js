7cb7a7a5620a7c969c936e42005d3a1e1b73c845
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-02-26 21:56:28 +0500

    Update passwordHashing.js

--- utils/passwordHashing.js
+++ utils/passwordHashing.js
@@ -1,10 +1,8 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-
 export const generatepasswordHash = (password,saltRounds)=>{ 
     return bcrypt.hash(password, saltRounds)
 }
-
 export const generateJwtToken = (data)=>{
     return jwt.sign(data, 'doctoAppointment');
 }
